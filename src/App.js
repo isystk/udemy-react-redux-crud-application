0170413a468b08012584e205d5e9f34b70e8f7af
iseyoshitaka <iseyoshitaka@team-lab.com>
2020-08-27 18:12:13 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <h1>Hello World</h1>
-  }
+// class App extends Component {
+//   render() {
+//     return <h1>Hello World</h1>
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meao</div>
 }
 
 export default App;
